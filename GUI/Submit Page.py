be7d446576c1de91526a0fa1cde5a281151812f5
Marwan Ehab Adel Mostafa <MarwanEhab754@gmail.com>
2025-11-22 21:16:33 +0200

    GUI V 6.0

--- GUI/Submit Page.py
+++ GUI/Submit Page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+import sys
 
 
 # ============================================================
@@ -55,6 +56,108 @@ if submitted:
     # Store form data
     st.session_state['Citizen'] = [name, email, phone, comment]
     st.toast("Form submitted successfully!", icon="✅")
+
+    # ============================================================
+    # FIX PYTHON PATH SO IMPORTS WORK FROM gui/main/
+    # ============================================================
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, "../../"))
+    sys.path.append(PROJECT_ROOT)
+
+    # ============================================================
+    # IMPORT YOUR PROJECT MODULES
+    # ============================================================
+    from dataEngineer.modeling.MLmodel2 import MultiTaskTextClassifier
+    from dataEngineer.pipeLine import *
+    from dataEngineer.modeling.Deeplearning2 import MultiOutputClassificationModel
+
+
+    MODEL_DIR = os.path.join(PROJECT_ROOT, "models/my_multi_task_models_afterCleaning_logostic")
+
+    tasks = ['problem_type', 'category']
+
+    # Load model
+    model = MultiTaskTextClassifier(
+        label_columns=tasks,
+        model_dir=MODEL_DIR,
+        model_type='logreg',
+        use_hyperparameter_tuning=True
+    )
+
+    # Predict
+    new_texts = [comment]
+    predictions = model.predict(new_texts)
+
+
+
+ 
+
+
+
+    # ============================================================
+    # DEEP LEARNING MODEL PREDICTION
+    # ============================================================
+
+
+    # ---- MODEL PATH ----
+    DL_MODEL_PATH = os.path.join(PROJECT_ROOT, "models/best_cv_classifier.pth")
+
+    # ---- GOOGLE DRIVE DIRECT FILE LINK (FIX THIS) ----
+    model_id = '1BHpawVMowc8D8yJAeFde6FS6Zq62OE07'
+    DL_MODEL_URL = f"https://drive.google.com/uc?id={model_id}"   # <-- replace with real ID
+
+    # ---- DOWNLOAD MODEL IF MISSING ----
+    if not os.path.exists(DL_MODEL_PATH):
+
+        gdown.download(DL_MODEL_URL, DL_MODEL_PATH, quiet=False)
+
+    # ---- LOAD MODEL (cached so it loads only once) ----
+ 
+    def load_dl_model():
+        return MultiOutputClassificationModel(
+            model_name='distilbert-base-uncased',
+            model_path=DL_MODEL_PATH
+        )
+
+    dl_model = load_dl_model()
+
+    # ---- RUN PREDICTION ----
+    dl_pred = dl_model.predict(comment)
+
+    Category_confidence = dl_pred['category']['confidence']
+    SubCategory_confidence = dl_pred['sub_category']['confidence']
+    Average_confidence = (Category_confidence + SubCategory_confidence) / 2
+    Ratio = (Category_confidence - SubCategory_confidence) / Category_confidence
+
+
+
+    # ============================================================
+    # ELT NEW DATA
+    # ============================================================
+
+
+    new_data = {
+            "category": dl_pred['category']['prediction'],
+            "subreddit": dl_pred['sub_category']['prediction'],
+            "problem_type": predictions['problem_type'][0],
+            "title": comment,
+            "text": comment
+        }
+
+
+    new_data_path = os.path.join(
+        os.path.dirname(__file__),
+        "../../data/new/new_data.csv"
+    )
+
+    if os.path.exists(new_data_path) and os.path.getsize(new_data_path) > 0:
+        df = pd.read_csv(new_data_path)
+    else:
+        df = pd.DataFrame(columns=["category", "subreddit", "problem_type", "title", "text"])
+    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
+    df.to_csv(new_data_path, index=False)
+
+
 
     
     
