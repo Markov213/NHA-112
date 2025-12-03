d777331251330e916b05e0d736b9ff300450db17
Marwan Ehab Adel Mostafa <MarwanEhab754@gmail.com>
2025-12-03 23:01:58 +0200

    test 7

--- GUI/pages/Dashboard Page (Present).py
+++ GUI/pages/Dashboard Page (Present).py
@@ -11,11 +11,8 @@ data_path = os.path.join(
 
 if os.path.exists(data_path) and os.path.getsize(data_path) > 0:
     # Reset Citizen state
-    st.session_state['Citizen'][0] = None
-    st.session_state['Citizen'][1] = None
-    st.session_state['Citizen'][2] = None
-    st.session_state['Citizen'][3] = None
-    
+    st.session_state.get('Citizen') = None
+
     df = pd.read_csv(data_path)
 
     st.title("People Complaints Dashboard")
